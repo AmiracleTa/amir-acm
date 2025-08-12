b3ddee6517e69c9c786ae4ffce388f5a15a005d2
AmiracleTa <amiracletm@gmail.com>
2025-08-12 23:08:44 +0800

    more functions

--- checker/_rng_utils.hpp
+++ checker/_rng_utils.hpp
@@ -11,8 +11,29 @@ const int inf = 0x3f3f3f3f;
 ull seed = chrono::steady_clock::now().time_since_epoch().count();
 mt19937_64 rng(seed);
 
+/*
+.Note
+  int range only. (
 
-// creat vector with n elements  ,  inclusive[lo, hi]
+.cur
+  number
+  array
+  permutation
+  01matrix with expectation
+
+  tree
+  simple (un)onnected graph
+
+
+*/
+
+// get one random number
+auto random_num(int lo, int hi){
+    uniform_int_distribution<int> dist(lo, hi);
+    int num = dist(rng);
+    return num;
+}
+// create vector with n elements  ,  inclusive[lo, hi]
 auto random_vec(int n, int lo, int hi){
     uniform_int_distribution<int> dist(lo, hi);
     vector<int> ans;
@@ -20,7 +41,7 @@ auto random_vec(int n, int lo, int hi){
     return ans;
 }
 
-// creat random permutation // 0-index
+// create random permutation // 0-index
 auto random_permutation_0_st(int n){ // [0, n-1]
     vector<int> p(n);
     iota(p.begin(), p.end(), 0);
@@ -33,9 +54,20 @@ auto random_permutation_1_st(int n){ // [1, n]
     shuffle(p.begin(), p.end(), rng);
     return p;
 }
+auto random_01mat(int n, int m, double exp=0.5){ // exp - P(X=1) - probability of 1 occurring - inclusive[0.0, 1.0]
+    vector<vector<int>> mat(n, vector<int>(m));
+    vector<double> wt{1-exp, exp}; // weight
+    discrete_distribution<int> dist(wt.begin(), wt.end());
+
+    for(auto& vec : mat) for(auto& x : vec){
+        x = dist(rng);
+    }
+
+    return mat;
+}
 
 // 只要把 [1..n] 的映射打乱, 就能得到完全随机的树
-// get a random tree
+// create a random tree
 auto random_tree(int n){ // n vertex
     vector<pii> edge;
     for(int i=2; i<=n; i++){
@@ -45,16 +77,41 @@ auto random_tree(int n){ // n vertex
     }
     return edge;
 }
+// create a  -simple undirected-  graph
+auto random_sim_gra(int n, int m){
+    assert( m <= 1ll*n*(n-1)/2 );
 
-// creat (n x m) 01 matrix
-auto random_01mat(int n, int m, double exp=0.5){ // exp - P(X=1) - probability of 1 occurring - inclusive[0.0, 1.0]
-    vector<vector<int>> mat(n, vector<int>(m));
-    vector<double> wt{1-exp, exp}; // weight
-    discrete_distribution<int> dist(wt.begin(), wt.end());
-    for(auto& vec : mat) for(auto& x : vec){
-        x = dist(rng);
+    auto edge = random_tree(n);
+    shuffle(edge.begin(), edge.end(), rng); 
+    int ext = max(0, n-1-m);
+    int del = random_num(ext, ext+min(n-1,m)/2); 
+    edge.erase(edge.end()-del, edge.end()); // delete extra edges && delete some edges randomly
+
+    set<pii> vis(edge.begin(), edge.end());
+    while(edge.size() < m){
+        auto vec = random_vec(2, 1, n);
+        int u = vec[0], v = vec[1];
+        if(u > v) swap(u, v);
+        if(u == v || vis.count({u, v})) continue;
+        edge.push_back({u, v});
+        vis.insert({u, v});
     }
-    return mat;
+    return edge;
 }
+// create a  -simple connected undirected-  graph
+auto random_sim_con_gra(int n, int m){ // 简单连通图 // 需要 m>=n-1
+    assert( n-1 <= m && m <= 1ll*n*(n-1)/2 );
 
+    auto edge = random_tree(n);
+    set<pii> vis(edge.begin(), edge.end());
+    while(edge.size() < m){
+        auto vec = random_vec(2, 1, n);
+        int u = vec[0], v = vec[1];
+        if(u > v) swap(u, v);
+        if(u == v || vis.count({u, v})) continue;
+        edge.push_back({u, v});
+        vis.insert({u, v});
+    }
+    return edge;
+}
 
